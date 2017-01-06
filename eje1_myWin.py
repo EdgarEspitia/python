cfc672b14bd6c8e4f3581fc5fc955774189891ce
fabian <fabian@localhost.localdomain>
2017-01-06 21:46:17 +0300

    checking the command set_position()

--- eje1_myWin.py
+++ eje1_myWin.py
@@ -8,6 +8,7 @@ class MyApplication(Gtk.Application):
         window = Gtk.Window(application=self)
         # set the title
         window.set_title("Welcome to GNOME from python")
+        #The command set_position doesn't work, I don't undestand whats happened!
         window.set_position(Gtk.WindowPosition.CENTER_ON_PARENT)
         print(window.get_resizable())
         # show the window
